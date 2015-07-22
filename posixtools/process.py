bd2cfd57a27e90c40a87b90440dc41ffa89a3aa6
Sungho Park <chywoo.park@samsung.com>
2015-07-22 11:15:54 +0900

    Initial commit

---
+++ posixtools/process.py
@@ -0,0 +1,74 @@
+import psutil
+import platform
+
+__author__ = 'chywoo.park'
+
+def kill(pid):
+    """
+    Kill process specified by pid paramter
+
+    :param pid: Process ID to kill
+    """
+    p = psutil.Process(pid)
+
+    try:
+        p.kill()
+    except Exception:
+        pass
+
+def killall(name, params=None):
+        """
+        Kill process specified by parameters.
+        If you input name only, the same name processes are killed.
+        If you input params too, this function checks the string is in the process command line and kills it.
+        params are list or str separated by ','.
+
+            >>> util.killall("emulator-x86")
+            >>> util.killall("java", "main.jar,string.jar")
+
+        :type name: Process name to kill
+        :type params: Filter string for Process command line. ex)params="main.jar,string.jar,/lib/usr.so"
+        """
+
+        if platform.system() == "Windows":
+            name += ".exe"
+
+        for ps in psutil.process_iter():
+            cmdline = ""
+            try:
+                if ps.name() != name:
+                    continue
+
+                if params:
+                    cmdline = ps.cmdline()
+            except psutil.AccessDenied:
+                continue
+
+            ps_found = True
+
+            if params:  # If you want to compare command line
+                check_list = []
+                if params is list:
+                    check_list = params
+                elif params is str:
+                    check_list = str.split(",")
+                else:
+                    check_list.append(str(params))
+
+                # Compare command line's parameters
+                for item in check_list:
+                    ps_found = False
+
+                    for param in cmdline:
+                        if param.find(item):
+                            ps_found = True
+                            break
+
+                    if ps_found is False:   # Process is not found.
+                        break
+
+            if ps_found:
+                try:
+                    ps.kill()
+                except Exception:
+                    pass
---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+import posixtools
+
+setup(
+    name=posixtools.__title__,
+    version=posixtools.__version__,
+    author=posixtools.__author__,
+    author_email='chywoo@gmail.com',
+    url='http://',
+    packages=[posixtools.__title__],
+    package_dir={posixtools.__title__:posixtools.__title__},
+    license='BSD New',
+    zip_safe=True,
+    classifiers=(
+        # 'Development Status :: 5 - Production/Unstable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD new',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'
+    ),
+    description = 'Posix tools for Python'
+    )
